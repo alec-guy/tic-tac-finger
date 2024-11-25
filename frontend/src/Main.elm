5b31b1bd217bd43b9e45f05f44fd13be73563588
alec <alecrodriguez1247@gmail.com>
2024-11-25 12:21:44 -0600

    working on front end

--- frontend/src/Main.elm
+++ frontend/src/Main.elm
@@ -17,3 +17,38 @@ main =
     , subscriptions = subscriptions
     , view = view
     }
+
+init : () -> (Model, Cmd Msg)
+init _ = (initialModel , Cmd.none)
+
+type alias Model = {shape : Mark}
+type Mark = X | Circle 
+oppositeMark : Mark -> Mark 
+oppositeMark m = if m == X then Circle else X
+type Msg = EnteredShape String 
+         | 
+update : Msg -> Model -> (Model, Cmd Msg)
+view : Model -> Html Msg 
+view model = grid 
+
+grid : Html Msg 
+grid = div
+       [class "grid-container"]
+       [ gridItem maybeMark
+       , gridItem maybeMark
+       , gridItem maybeMark
+       , gridItem maybeMark
+       , gridItem maybeMark
+       , gridItem maybeMark
+       , gridItem maybeMark
+       , gridItem maybeMark
+       , gridItem maybeMark
+       ]
+gridItem1 : Maybe Mark -> Html Msg 
+gridItem maybeMark = div 
+                     [class "grid-item"]
+                     [case maybeMark of 
+                       Nothing     -> text ""
+                       (Just mark) -> showMark mark
+                     ]
+showMark : Mark -> Html Msg 
